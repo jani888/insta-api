59ab65990d002c8f8aa768b7a7b554f586c201d2
Hidvégi János <jani.hidvegi@gmail.com>
2019-06-05 20:01:10 +0200

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsersTableSeeder::class]);
+        $user = User::create(['name' => 'jani', 'email' => 'jani.hidvegi@gmail.com', 'password' => bcrypt('admin')]);
+        $account = factory(\App\Models\InstagramAccount::class)->create();
+        $user->accounts()->attach($account);
+        $hashtags = factory(App\Models\Hashtag::class)->times(10)->create();
+        $account->hashtags()->sync($hashtags->random(3)->pluck('id'));
+        //factory(\App\Models\Post::class)->times(50)->create();
     }
 }
