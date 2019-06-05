a32c78de65ff0c331b2bf1de4375793c11cfe5aa
Hidvégi János <jani.hidvegi@gmail.com>
2019-06-05 21:51:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::resource('user', 'UserController', ['except' => ['show']]);
 
-	Route::resource('accounts', 'InstagramAcountController', ['as' => 'accounts.create', 'except' => ['show']]);
+	Route::resource('accounts', 'InstagramAcountController', ['name' => 'accounts.create', 'except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
